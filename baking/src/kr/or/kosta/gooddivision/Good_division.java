6a5fc8195ebbca54e13b217e51025ca49e5ce4d7
cjh5390 <cjh5390@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-26 08:36:11 +0000

    Good_division추가

--- baking/src/kr/or/kosta/gooddivision/Good_division.java
+++ baking/src/kr/or/kosta/gooddivision/Good_division.java
@@ -14,5 +14,27 @@ public class Good_division {
 	 */
 	private String gName;
 
+	public int getDivision() {
+		return division;
+	}
+
+	public void setDivision(int division) {
+		this.division = division;
+	}
+
+	public String getgName() {
+		return gName;
+	}
+
+	public void setgName(String gName) {
+		this.gName = gName;
+	}
+
+	@Override
+	public String toString() {
+		return "Good_division [division=" + division + ", gName=" + gName + "]";
+	}
+
 //	private Good good_division;
+	
 }
