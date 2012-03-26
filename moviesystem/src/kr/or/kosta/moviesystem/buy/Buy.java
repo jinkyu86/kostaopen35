673175832f9d2744ecf714f48eabf4c91597f850
sunheePaek <sunheePaek@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-26 08:32:59 +0000

    buy Vo등록

--- moviesystem/src/kr/or/kosta/moviesystem/buy/Buy.java
+++ moviesystem/src/kr/or/kosta/moviesystem/buy/Buy.java
@@ -2,11 +2,14 @@ package kr.or.kosta.moviesystem.buy;
 
 import java.util.Date;
 
+import kr.or.kosta.moviesystem.good.Good;
+import kr.or.kosta.moviesystem.member.Member;
+
 public class Buy {
 
-	public Good good;
+	private Good good;
 
-	public Member member;
+	private Member member;
 
 	/**
 	 * 구매번호
@@ -32,4 +35,68 @@ public class Buy {
 	 * 전체 가격
 	 */
 	private Number totalPrice;
+
+	public Good getGood() {
+		return good;
+	}
+
+	public void setGood(Good good) {
+		this.good = good;
+	}
+
+	public Member getMember() {
+		return member;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
+	}
+
+	public Number getBuynum() {
+		return buynum;
+	}
+
+	public void setBuynum(Number buynum) {
+		this.buynum = buynum;
+	}
+
+	public Number getQty() {
+		return qty;
+	}
+
+	public void setQty(Number qty) {
+		this.qty = qty;
+	}
+
+	public Date getBuyDate() {
+		return buyDate;
+	}
+
+	public void setBuyDate(Date buyDate) {
+		this.buyDate = buyDate;
+	}
+
+	public Number getPayState() {
+		return payState;
+	}
+
+	public void setPayState(Number payState) {
+		this.payState = payState;
+	}
+
+	public Number getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(Number totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "Buy [good=" + good + ", member=" + member + ", buynum="
+				+ buynum + ", qty=" + qty + ", buyDate=" + buyDate
+				+ ", payState=" + payState + ", totalPrice=" + totalPrice + "]";
+	}
+	
 }
