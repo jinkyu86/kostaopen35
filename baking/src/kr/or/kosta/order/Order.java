bb97aa1ab144ef1ff9634976c319de10de332fd5
jjunggyu <jjunggyu@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-26 08:32:46 +0000

    추가

--- baking/src/kr/or/kosta/order/Order.java
+++ baking/src/kr/or/kosta/order/Order.java
@@ -41,4 +41,76 @@ public class Order {
 	 * 주문일자
 	 */
 	private Date buyDate;
+
+	public Member getMember() {
+		return member;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
+	}
+
+	public Good getGood() {
+		return good;
+	}
+
+	public void setGood(Good good) {
+		this.good = good;
+	}
+
+	public int getOrderNum() {
+		return orderNum;
+	}
+
+	public void setOrderNum(int orderNum) {
+		this.orderNum = orderNum;
+	}
+
+	public String getMemberid() {
+		return memberid;
+	}
+
+	public void setMemberid(String memberid) {
+		this.memberid = memberid;
+	}
+
+	public int getGoodNum() {
+		return goodNum;
+	}
+
+	public void setGoodNum(int goodNum) {
+		this.goodNum = goodNum;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public Date getBuyDate() {
+		return buyDate;
+	}
+
+	public void setBuyDate(Date buyDate) {
+		this.buyDate = buyDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [member=" + member + ", good=" + good + ", orderNum="
+				+ orderNum + ", memberid=" + memberid + ", goodNum=" + goodNum
+				+ ", qty=" + qty + ", price=" + price + ", buyDate=" + buyDate
+				+ "]";
+	}
 }
