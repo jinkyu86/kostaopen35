ab16596f481c96959cfd31da6e2879c1e766625f
cjh5390 <cjh5390@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-26 08:36:23 +0000

    Member추가

--- baking/src/kr/or/kosta/member/Member.java
+++ baking/src/kr/or/kosta/member/Member.java
@@ -65,7 +65,114 @@ public class Member {
 	 */
 	private String telNumber;
 
+	public String getMemberid() {
+		return memberid;
+	}
+
+	public void setMemberid(String memberid) {
+		this.memberid = memberid;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRegiNumber() {
+		return regiNumber;
+	}
+
+	public void setRegiNumber(String regiNumber) {
+		this.regiNumber = regiNumber;
+	}
+
+	public String getPwHint() {
+		return pwHint;
+	}
+
+	public void setPwHint(String pwHint) {
+		this.pwHint = pwHint;
+	}
+
+	public String getPwAnswer() {
+		return pwAnswer;
+	}
+
+	public void setPwAnswer(String pwAnswer) {
+		this.pwAnswer = pwAnswer;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getStrAddress() {
+		return strAddress;
+	}
+
+	public void setStrAddress(String strAddress) {
+		this.strAddress = strAddress;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getTelNumber() {
+		return telNumber;
+	}
+
+	public void setTelNumber(String telNumber) {
+		this.telNumber = telNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [memberid=" + memberid + ", password=" + password
+				+ ", name=" + name + ", regiNumber=" + regiNumber + ", pwHint="
+				+ pwHint + ", pwAnswer=" + pwAnswer + ", zipcode=" + zipcode
+				+ ", address=" + address + ", strAddress=" + strAddress
+				+ ", email=" + email + ", phoneNumber=" + phoneNumber
+				+ ", telNumber=" + telNumber + "]";
+	}
+
 //	private Order member;
 
 //	private Myrecipe member;
+	
 }
