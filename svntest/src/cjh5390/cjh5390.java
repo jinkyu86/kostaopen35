2d2ac64761d8c804f167b6e071a9c21a1373be1a
cjh5390 <cjh5390@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-26 02:31:19 +0000

    create class

---
+++ svntest/src/cjh5390/cjh5390.java
@@ -0,0 +1,13 @@
+package cjh5390;
+
+public class cjh5390 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("cjh5390");
+	}
+
+}
