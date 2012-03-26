a1eff835474c0984ce69d9d37bec4648d250afbe
kazg86 <kazg86@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-26 08:40:39 +0000

    member getter setter, tostring

--- auction/src/kr/or/kosta/auction/member/Member.java
+++ auction/src/kr/or/kosta/auction/member/Member.java
@@ -36,9 +36,83 @@ public class Member {
 	 */
 	private String emoney;
 
-	private Auction auction;
+
 
 	private Bid bid;
 
 	private Board board;
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCoin() {
+		return coin;
+	}
+
+	public void setCoin(String coin) {
+		this.coin = coin;
+	}
+
+	public String getEmoney() {
+		return emoney;
+	}
+
+	public void setEmoney(String emoney) {
+		this.emoney = emoney;
+	}
+
+	public Bid getBid() {
+		return bid;
+	}
+
+	public void setBid(Bid bid) {
+		this.bid = bid;
+	}
+
+	public Board getBoard() {
+		return board;
+	}
+
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [userid=" + userid + ", pw=" + pw + ", email=" + email
+				+ ", name=" + name + ", coin=" + coin + ", emoney=" + emoney
+				+ ", bid=" + bid + ", board=" + board
+				+ "]";
+	}
+	
+	
 }
