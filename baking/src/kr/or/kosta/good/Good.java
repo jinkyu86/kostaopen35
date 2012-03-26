8ef6e67fe9543c7e5f24a9e53254e8b30b3f3f3d
cjh5390 <cjh5390@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-26 08:35:56 +0000

    Good 추가

--- baking/src/kr/or/kosta/good/Good.java
+++ baking/src/kr/or/kosta/good/Good.java
@@ -48,4 +48,93 @@ public class Good {
 	 */
 	private String option;
 
+	public Good_division getGood_division() {
+		return good_division;
+	}
+
+	public void setGood_division(Good_division good_division) {
+		this.good_division = good_division;
+	}
+
+	public Recipe getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+
+	public int getGoodNum() {
+		return goodNum;
+	}
+
+	public void setGoodNum(int goodNum) {
+		this.goodNum = goodNum;
+	}
+
+	public int getDivision() {
+		return division;
+	}
+
+	public void setDivision(int division) {
+		this.division = division;
+	}
+
+	public int getGoodPrice() {
+		return goodPrice;
+	}
+
+	public void setGoodPrice(int goodPrice) {
+		this.goodPrice = goodPrice;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getExplantion() {
+		return explantion;
+	}
+
+	public void setExplantion(String explantion) {
+		this.explantion = explantion;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getOption() {
+		return option;
+	}
+
+	public void setOption(String option) {
+		this.option = option;
+	}
+
+	@Override
+	public String toString() {
+		return "Good [good_division=" + good_division + ", recipe=" + recipe
+				+ ", goodNum=" + goodNum + ", division=" + division
+				+ ", goodPrice=" + goodPrice + ", qty=" + qty + ", name="
+				+ name + ", explantion=" + explantion + ", img=" + img
+				+ ", option=" + option + "]";
+	}
+	
 }
