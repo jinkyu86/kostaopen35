1cf0c860ba8bf6e093d47569a864c9896518a880
jjunggyu <jjunggyu@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-26 08:33:02 +0000

    추가

--- baking/src/kr/or/kosta/recipe/Recipe.java
+++ baking/src/kr/or/kosta/recipe/Recipe.java
@@ -26,4 +26,51 @@ public class Recipe {
 
 	private Good good;
 
+	public int getRecipeNum() {
+		return recipeNum;
+	}
+
+	public void setRecipeNum(int recipeNum) {
+		this.recipeNum = recipeNum;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public Good getGood() {
+		return good;
+	}
+
+	public void setGood(Good good) {
+		this.good = good;
+	}
+
+	@Override
+	public String toString() {
+		return "Recipe [recipeNum=" + recipeNum + ", title=" + title
+				+ ", content=" + content + ", img=" + img + ", good=" + good
+				+ "]";
+	}
+
 }
