5b45e9c749d03f4f55ef287883e9799e662e44b1
sanghee00s <sanghee00s@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-26 08:33:45 +0000

    member VO 클래스 수정

--- moviesystem/src/kr/or/kosta/moviesystem/member/Member.java
+++ moviesystem/src/kr/or/kosta/moviesystem/member/Member.java
@@ -64,7 +64,111 @@ public class Member {
 	 */
 	private Date dropDate;
 
-	private Reservation member;
+	public String getUserid() {
+		return userid;
+	}
 
-	private Buy member;
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public Number getUserNum() {
+		return userNum;
+	}
+
+	public void setUserNum(Number userNum) {
+		this.userNum = userNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+	public String getMemState() {
+		return memState;
+	}
+
+	public void setMemState(String memState) {
+		this.memState = memState;
+	}
+
+	public String getDropReason() {
+		return dropReason;
+	}
+
+	public void setDropReason(String dropReason) {
+		this.dropReason = dropReason;
+	}
+
+	public Date getDropDate() {
+		return dropDate;
+	}
+
+	public void setDropDate(Date dropDate) {
+		this.dropDate = dropDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [userid=" + userid + ", userNum=" + userNum + ", name="
+				+ name + ", pw=" + pw + ", email=" + email + ", phone=" + phone
+				+ ", zipcode=" + zipcode + ", addr=" + addr + ", regDate="
+				+ regDate + ", memState=" + memState + ", dropReason="
+				+ dropReason + ", dropDate=" + dropDate + "]";
+	}
+	
+	
+
 }
