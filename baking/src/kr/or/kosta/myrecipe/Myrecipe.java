5305f2fa1ec84b0a997918ee301b538eba2bd12d
jjunggyu <jjunggyu@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-26 08:32:26 +0000

    추가

--- baking/src/kr/or/kosta/myrecipe/Myrecipe.java
+++ baking/src/kr/or/kosta/myrecipe/Myrecipe.java
@@ -43,4 +43,76 @@ public class Myrecipe {
 	 * ±Û³»¿ë
 	 */
 	private String content;
+
+	public Member getMember() {
+		return member;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
+	}
+
+	public int getBoardNum() {
+		return boardNum;
+	}
+
+	public void setBoardNum(int boardNum) {
+		this.boardNum = boardNum;
+	}
+
+	public String getMemberid() {
+		return memberid;
+	}
+
+	public void setMemberid(String memberid) {
+		this.memberid = memberid;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public String toString() {
+		return "Myrecipe [member=" + member + ", boardNum=" + boardNum
+				+ ", memberid=" + memberid + ", title=" + title + ", date="
+				+ date + ", count=" + count + ", img=" + img + ", content="
+				+ content + "]";
+	}
 }
