da78941a935c4c7f28f43c00b4b0e4fa31d2616f
wanseub0909 <wanseub0909@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-27 00:40:17 +0000

    클래스 수정

--- bookchange/src/kr/or/kosta/change/Condition.java
+++ bookchange/src/kr/or/kosta/change/Condition.java
@@ -1,16 +1,29 @@
 package kr.or.kosta.change;
 
-import kr.or.kosta.bookchange.board.Board;
 
 public class Condition {
 
 	/**교환결과(0-교환가능, 1-교환중, 2-교환완료)**/
 	private int conditionResult;
-
-	/**교환처리상태(교환가능,교환중,교환완료)**/
 	private String conditionIng;
-
-	private Change changeCondition;//교환상태
-
-	private Board boardCondition;//현재교환상태
+	
+	public int getConditionResult() {
+		return conditionResult;
+	}
+	public void setConditionResult(int conditionResult) {
+		this.conditionResult = conditionResult;
+	}
+	public String getConditionIng() {
+		return conditionIng;
+	}
+	public void setConditionIng(String conditionIng) {
+		this.conditionIng = conditionIng;
+	}
+	@Override
+	public String toString() {
+		return "Condition [conditionResult=" + conditionResult
+				+ ", conditionIng=" + conditionIng + "]";
+	}
+	
+	
 }
