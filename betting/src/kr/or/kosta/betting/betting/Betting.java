6bee71463c12f535afc2f461a5b36322258a68ed
lhkgogo <lhkgogo@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-27 05:31:13 +0000

    메서드 작성

--- betting/src/kr/or/kosta/betting/betting/Betting.java
+++ betting/src/kr/or/kosta/betting/betting/Betting.java
@@ -41,5 +41,77 @@ public class Betting {
 	 */
 	private String teamNum;
 
+	public Match getMatch() {
+		return match;
+	}
 
+	public void setMatch(Match match) {
+		this.match = match;
+	}
+
+	public String getNum() {
+		return num;
+	}
+
+	public void setNum(String num) {
+		this.num = num;
+	}
+
+	public String getMatchNum() {
+		return matchNum;
+	}
+
+	public void setMatchNum(String matchNum) {
+		this.matchNum = matchNum;
+	}
+
+	public long getBatRating() {
+		return batRating;
+	}
+
+	public void setBatRating(long batRating) {
+		this.batRating = batRating;
+	}
+
+	public long getSeleRating() {
+		return seleRating;
+	}
+
+	public void setSeleRating(long seleRating) {
+		this.seleRating = seleRating;
+	}
+
+	public long getTotMineral() {
+		return totMineral;
+	}
+
+	public void setTotMineral(long totMineral) {
+		this.totMineral = totMineral;
+	}
+
+	public String getDistnum() {
+		return distnum;
+	}
+
+	public void setDistnum(String distnum) {
+		this.distnum = distnum;
+	}
+
+	public String getTeamNum() {
+		return teamNum;
+	}
+
+	public void setTeamNum(String teamNum) {
+		this.teamNum = teamNum;
+	}
+
+	@Override
+	public String toString() {
+		return "Betting [match=" + match + ", num=" + num + ", matchNum="
+				+ matchNum + ", batRating=" + batRating + ", seleRating="
+				+ seleRating + ", totMineral=" + totMineral + ", distnum="
+				+ distnum + ", teamNum=" + teamNum + "]";
+	}
+
+	
 }
