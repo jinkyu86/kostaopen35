ae47a7cf5f4c9d84d03c0701bf010e3b7d5ff5e4
kimkijeong1@naver.com <kimkijeong1@naver.com@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-28 01:10:13 +0000

    bv

--- moviesystem/src/kr/or/kosta/moviesystem/reservation/ReservationDAO.java
+++ moviesystem/src/kr/or/kosta/moviesystem/reservation/ReservationDAO.java
@@ -10,10 +10,12 @@ import java.util.ArrayList;
 
 
 
+import kr.or.kosta.department.Department;
 import kr.or.kosta.moviesystem.member.Member;
 import kr.or.kosta.moviesystem.movie.Movie;
 import kr.or.kosta.moviesystem.screentime.ScreenTime;
 import kr.or.kosta.moviesystem.util.ConnectionUtil;
+import kr.or.kosta.student.Student;
 
 
 
@@ -47,6 +49,64 @@ public class ReservationDAO {
 		}
 	}
 
+	
+	
+	
+	public static ArrayList<Reservation> selectReservationList(int length,int page,String memberid){//반환값을 ArrayList로 한다.
+		Connection con =null;
+		PreparedStatement psmt=null;
+		String sql=null;//쿼리문 저장할 곳
+		ResultSet rs=null;//커리문의 주소를 받아온다.(rs.next()는 쿼리문이있으면 true 없으면 flase
+		Reservation reservation=null;
+		ArrayList<Reservation>reservationList=new ArrayList<Reservation>();
+		try{
+			con=ConnectionUtil.getConnection();
+			sql="SELECT   m.m_name,s.time,r.seatnum,r.res_date" +
+					
+					"  FROM  RESERVATION r ,MEMBER mem"+
+					"                   MOVIE m,SCREENING_TIME s " +
+					"  WHERE  m.userid=r.userid AND  r.scr_num=s.scr_num" +
+					"                      AND r.m_num=m.m_num " +
+					"                      AND  m.userid=?" +
+					"    ORDER BY res_date  DESC";
+			//rs.absolute()가  가능하도록 설정
+			psmt=con.prepareStatement(sql,
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			psmt.setString(1,"%"+memberid+"%");
+			rs=psmt.executeQuery();//쿼리 결과를 rs에 저장
+			if(page>1){
+				rs.absolute((page-1)*length);
+			}
+			//가져온 레코드 개수
+			int getRecordCount=0;
+			while(rs.next()){
+				String m_name=rs.getString(1);
+				String time=rs.getString(2);
+				long seatnum=rs.getShort(3);					
+				Date resDate=rs.getDate(4);
+								
+				reservation=new Reservation();
+				
+				Movie movie=new Movie();
+				ScreenTime screentime=new ScreenTime();
+				
+				
+				movie.setMname(m_name);
+				reservation.setMovie(movie);
+				screentime.setTime(time);
+				reservation.setScreenTime(screentime);
+				reservation.setSeatnum(seatnum);
+				reservation.setResDate(resDate);
+				
+			
+				reservationList.add(reservation);
+			}//end while
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		return reservationList;
+	}
 	/**
 	 * 회원아이디로 예매내역을 찾을 수 있는 메서드
 
@@ -60,14 +120,14 @@ public class ReservationDAO {
 		ArrayList<Reservation>reservationList=new ArrayList<Reservation>();
 		try {
 			con=ConnectionUtil.getConnection();
-			sql="SELECT   m.m_name,s.time,r.seatnum,r.res_date" +
+			sql="SELECT   m.m_name,s.time,r.seat_num,r.res_date" +
 					
-					"  FROM  RESERVATION r ,MEMBER mem"+
+					"  FROM  RESERVATION r ,MEMBER mem,"+
 					"                   MOVIE m,SCREENING_TIME s " +
-					"  WHERE  m.userid=r.userid AND  r.scr_num=s.scr_num" +
+					"  WHERE  mem.userid=r.userid AND  r.scr_num=s.scr_num" +
 					"                      AND r.m_num=m.m_num " +
-					"                      AND  m.userid=?" +
-					"    ORDER BY res_date  DESC";
+					"                      AND  r.userid=?" +
+					"    ORDER BY r.res_date  DESC";
 			
 			
 				psmt=con.prepareStatement(sql);
@@ -104,11 +164,32 @@ public class ReservationDAO {
 	/**
 	 * 회원아이디로 찾은 예매목록의 수를 찾을 수 있는 기능
 	 * 
-	 * @param userid
 	 */
 	public int selectReservationCount(String userid) {
-		/* default generated stub */;
-		return 0;
+		Connection con =null;
+		PreparedStatement psmt=null;
+		String sql=null;//쿼리문 저장할 곳
+		ResultSet rs=null;//커리문의 주소를 받아온다.(rs.next()는 쿼리문이있으면 true 없으면 flase
+		int reservationCount=0;
+		try{
+			con=ConnectionUtil.getConnection();
+			sql="SELECT count(userid)"+
+			       " From reservation"+
+				   " WHERE userid=?";
+			//rs.absolute()가  가능하도록 설정
+			psmt=con.prepareStatement(sql);
+			psmt.setString(1,userid);
+			rs=psmt.executeQuery();//쿼리 결과를 rs에 저장
+			
+			
+			if(rs.next()){
+				reservationCount=rs.getInt(1);
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		return reservationCount;
+		
 	}
 
 	/**
@@ -141,7 +222,7 @@ public class ReservationDAO {
 	 * @param userid
 	 * @param resnum
 	 */
-	public void removeReservation(Reservation reservation) {
+	public static void removeReservation(Reservation reservation) {
 		 Connection con=null;
 		  PreparedStatement psmt=null;
 		  
