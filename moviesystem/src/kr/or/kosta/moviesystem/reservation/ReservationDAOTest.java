6d7f367b29d31b58ccc4b2a98c5145119a4daf90
kimkijeong1@naver.com <kimkijeong1@naver.com@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-28 01:10:01 +0000

    dfs

--- moviesystem/src/kr/or/kosta/moviesystem/reservation/ReservationDAOTest.java
+++ moviesystem/src/kr/or/kosta/moviesystem/reservation/ReservationDAOTest.java
@@ -12,62 +12,73 @@ import org.junit.Test;
 
 public class ReservationDAOTest {
 
+//	@Test
+//	public void testInsertReservation() {
+//		Reservation reservation = new Reservation();
+//		
+//		Movie movie =new Movie();
+//		movie.setMnum("1");
+//		reservation.setMovie(movie);
+//		
+//		Member member=new Member();
+//		member.setUserid("jun123");
+//		reservation.setMember(member);
+//		
+//		ScreenTime screenTime=new ScreenTime();
+//		screenTime.setScrnum("1");
+//		reservation.setScreenTime(screenTime);
+//		reservation.setResQty(1);
+//		reservation.setTotalPrice(8000);
+//		reservation.setPayState(1);
+//		reservation.setSeatnum(28);
+//		
+//		ReservationDAO.insertReservation(reservation);
+//	}
+//	@Test
+//	public void testUpdateReservation(){
+//		Reservation reservation = new Reservation();
+//		
+//		Movie movie =new Movie();
+//		movie.setMnum("2");
+//		reservation.setMovie(movie);
+//		
+//		ScreenTime screenTime=new ScreenTime();
+//		screenTime.setScrnum("2");
+//		reservation.setScreenTime(screenTime);
+//		
+//		reservation.setResnum("1");
+//		
+//		ReservationDAO.updateReservation(reservation);
+//		
+//	}
+//	@Test
+//	public void testRemoveReservation(){
+//		Reservation reservation = new Reservation();
+//		
+//		reservation.setPayState(0);
+//		reservation.setResnum("2");
+//		
+//		ReservationDAO.removeReservation(reservation);
+//		
+//	}
+////	@Test
+////	public void testDeleteStudent(){
+////		StudentDAO.deleteStudent("40101");
+////		
+////	}
 	@Test
-	public void testInsertReservation() {
-		Reservation reservation = new Reservation();
-		
-		Movie movie =new Movie();
-		movie.setMnum("1");
-		reservation.setMovie(movie);
-		
-		Member member=new Member();
-		member.setUserid("jun123");
-		reservation.setMember(member);
-		
-		ScreenTime screenTime=new ScreenTime();
-		screenTime.setScrnum("1");
-		reservation.setScreenTime(screenTime);
-		reservation.setResQty(1);
-		reservation.setTotalPrice(8000);
-		reservation.setPayState(1);
-		reservation.setSeatnum(28);
-		
-		ReservationDAO.insertReservation(reservation);
-	}
-//	@Test
-//	public void testUpdateStudent(){
-//		Student student = new Student();
-//		student.setStudno("40101");
-//		student.setName("È«±æ¼ø");
-//		student.setUserid("gilsun");
-//		student.setPw("0070");
-//		
-//		Department department = new Department();
-//		department.setDeptno("102");
-//		
-//		student.setDepartment(department);
-//		
-//		StudentDAO.updateStudent(student);
-//		
-//	}
-//	@Test
-//	public void testDeleteStudent(){
-//		StudentDAO.deleteStudent("40101");
-//		
-//	}
-//	@Test
-//	public void testStudentList(){
-//		ArrayList<Student>studentList = StudentDAO.selectStudentList();
-//		for(int i =0;i<studentList.size();i++){
-//			Student student = studentList.get(i);
-//			System.out.println(student);
-//		}//end for
-//	}//end method
-//	@Test
-//	public void testSelectstudent(){
-//		
-//		Student student = StudentDAO.selectStudent("10201");
-//		System.out.println(student);
-//		
-//	}
+	public void testReservationList(){
+		ArrayList<Reservation>reservationList = ReservationDAO.selectReservationList("jun123");
+		for(int i =0;i<reservationList.size();i++){
+			Reservation reservation = reservationList.get(i);
+			System.out.println(reservation);
+		}//end for
+	}//end method
+////	@Test
+////	public void testSelectstudent(){
+////		
+////		Student student = StudentDAO.selectStudent("10201");
+////		System.out.println(student);
+////		
+////	}
 }//end class
