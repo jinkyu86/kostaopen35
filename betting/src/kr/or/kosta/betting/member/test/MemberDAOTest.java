d8c88ad32fc863268849f1b7877ccbff7a8bae04
lhkgogo <lhkgogo@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-29 08:44:59 +0000

    테스트 추가

---
+++ betting/src/kr/or/kosta/betting/member/test/MemberDAOTest.java
@@ -0,0 +1,73 @@
+package kr.or.kosta.betting.member.test;
+
+import java.util.ArrayList;
+
+import kr.or.kosta.betting.member.Member;
+import kr.or.kosta.betting.member.MemberDAO;
+
+import org.junit.Test;
+
+public class MemberDAOTest {
+
+	@Test
+	public void testSelectMemberListIntInt() {
+		ArrayList<Member>page1List=
+				MemberDAO.selectMemberList(5, 1);
+		System.out.println("page1List:"+page1List);
+		ArrayList<Member>page2List=
+				MemberDAO.selectMemberList(5, 2);
+		System.out.println("page2List:"+page2List);
+	}
+
+	@Test
+	public void testInsertMember() {
+		
+		Member member = new Member();
+		member.setID("jun123");
+		member.setName("¾ÆÀú¾¾");
+		member.setPW("1234");
+		member.setMineral(1000);
+		member.setEmail("jun1");
+		
+		MemberDAO.insultMember(member);
+		
+	}
+	@Test
+	public void testDeleteMember(){
+		
+		MemberDAO.deleteMember("jun123");
+		
+		
+	}
+	@Test
+	public void selectMemberByID(){
+		Member member =MemberDAO.selectMemberByID("jun1");
+		
+		System.out.println(member);
+		
+		
+	}
+	@Test
+	public void selectMemberRankingList(){
+		
+		ArrayList<Member>page1List=
+				MemberDAO.selectMemberRankingList(5, 1);
+		System.out.println("page1List:"+page1List);
+		ArrayList<Member>page2List=
+				MemberDAO.selectMemberRankingList(5, 2);
+		System.out.println("page2List:"+page2List);
+		
+	}
+	@Test
+	public void updateMember(){
+		
+		Member member = new Member();
+		member.setID("jun123");
+		member.setPW("1234");
+		member.setMineral(1000);
+		member.setEmail("jun21");
+		
+		MemberDAO.updateMember(member);
+		
+	}
+}
