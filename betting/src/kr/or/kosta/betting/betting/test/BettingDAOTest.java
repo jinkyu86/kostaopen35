fb31bc074adc46d894f4b5590e908bcaea5c4e81
lhkgogo <lhkgogo@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-29 08:40:12 +0000

    test 클레스 추가

---
+++ betting/src/kr/or/kosta/betting/betting/test/BettingDAOTest.java
@@ -0,0 +1,83 @@
+package kr.or.kosta.betting.betting.test;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import kr.or.kosta.betting.betting.Betting;
+import kr.or.kosta.betting.betting.BettingDAO;
+import kr.or.kosta.betting.match.Match;
+import kr.or.kosta.betting.team.Team;
+
+public class BettingDAOTest extends BettingDAO {
+
+
+	@Test
+	public void testInsertHomeBetting(){
+		Betting betting = new Betting();
+		
+		Match match = new Match();
+		match.setNum("10012");
+	
+		betting.setMatch(match);
+		
+		BettingDAO.insertHomeBetting(betting);
+	}
+	@Test
+	public void testInsertAwayBetting() {
+		Betting betting = new Betting();
+
+		Match match = new Match();
+		match.setNum("10012");
+	
+		betting.setMatch(match);
+		
+		BettingDAO.insertAwayBetting(betting);
+	}
+	@Test
+	public void testSelectBettingList() {
+		ArrayList<Betting> page1List = BettingDAO.selectBettingList(1, 5);
+		System.out.println("page1List:" + page1List);
+		ArrayList<Betting> page2List = BettingDAO.selectBettingList(2, 5);
+		System.out.println("page2List:" + page2List);
+	}
+	@Test
+	public void testSelectBettingListByDate(){
+		ArrayList<Betting> bettingList = BettingDAO.selectBettingListByDate("2012/03/18");
+		for(int i=0;i<bettingList.size();i++){
+			Betting betting = bettingList.get(i);
+		  System.out.println(betting);
+		}
+	}
+	@Test
+	public void testSelectBettingRating(){
+		long bettingRating = BettingDAO.selectBettingRating("5");
+		System.out.println(bettingRating);
+	}
+	@Test
+	public void testSelectBettingSeleRating(){
+		long bettingSeleRating = BettingDAO.selectBettingSeleRating("5");
+		System.out.println(bettingSeleRating);
+	}
+	@Test
+	public void testSelectBettingTotMineral(){
+		long bettingTotMineral = BettingDAO.selectBettingTotMineral("5");
+		System.out.println(bettingTotMineral);
+	}
+	@Test
+	public void testUpdateBetting(){
+		Betting betting = new Betting();
+		betting.setNum("3");
+		betting.setSeleRating(1);
+		betting.setTotMineral(1);
+		betting.setBatRating(1);
+		betting.setDistnum("1");
+
+		Match match = new Match();
+		match.setNum("10000");
+					
+		betting.setMatch(match);
+		
+		BettingDAO.updateBetting(betting);
+	}
+}
