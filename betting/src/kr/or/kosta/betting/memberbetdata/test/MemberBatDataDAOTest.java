b2cb5ebe2b81f4501a5d051fcd3fa13f86ef7c3e
lhkgogo <lhkgogo@31618103-7477-875a-9d44-c0bc0b56f092>
2012-03-29 07:45:55 +0000

    test 패키지 추가

---
+++ betting/src/kr/or/kosta/betting/memberbetdata/test/MemberBatDataDAOTest.java
@@ -0,0 +1,63 @@
+package kr.or.kosta.betting.memberbetdata.test;
+
+import java.util.ArrayList;
+
+import kr.or.kosta.betting.betting.Betting;
+import kr.or.kosta.betting.member.Member;
+import kr.or.kosta.betting.memberbetdata.MemberBatData;
+import kr.or.kosta.betting.memberbetdata.MemberBetDataDAO;
+
+import org.junit.Test;
+
+public class MemberBatDataDAOTest {
+
+	@Test
+	public void testInsultMemberBetData(){
+		MemberBatData mbd = new MemberBatData();
+		mbd.setBetMineral(100);
+		
+		Member member = new Member();
+		member.setID("jun1");
+		
+		Betting betting = new Betting();
+		betting.setNum("5");
+		
+		mbd.setBetting(betting);
+		mbd.setMember(member);
+		
+		MemberBetDataDAO.insultMemberBetData(mbd);
+	}
+	@Test
+	public void testSelectMemberBetDataList(){
+		ArrayList<MemberBatData> page1List = 
+				MemberBetDataDAO.selectMemberBetDataList(1, 5);
+		System.out.println("page1List:" + page1List);
+		ArrayList<MemberBatData> page2List = 
+				MemberBetDataDAO.selectMemberBetDataList(2, 5);
+		System.out.println("page2List:" + page2List);
+	}
+	@Test
+	public void testSelectMemberBetDataListByID(){
+		ArrayList<MemberBatData> page1List = 
+				MemberBetDataDAO.selectMemberBetDataListByID(1,5,"jun1");
+		System.out.println("page1List:" + page1List);
+		ArrayList<MemberBatData> page2List = 
+				MemberBetDataDAO.selectMemberBetDataListByID(2,5,"jun1");
+		System.out.println("page2List:" + page2List);
+	}
+	@Test
+	public void testDeleteMemberBetData(){
+		MemberBetDataDAO.deleteMemberbetData("16");
+		
+	}
+	@Test
+	public void testSelectMemberBetDataByIDCount(){
+		long idcount = MemberBetDataDAO
+					.selectMemberBetDataByIDCount("jun1");
+		System.out.println(idcount);
+	}
+	@Test
+	public void testUpdateMemberBatData(){
+		MemberBetDataDAO.updateMemberBetData("14");
+	}
+}
