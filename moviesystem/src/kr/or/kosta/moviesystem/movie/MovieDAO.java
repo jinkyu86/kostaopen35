3ccefcbc2b463241369b7d1e3e73c7b805d951df
chieka@naver.com <chieka@naver.com@31618103-7477-875a-9d44-c0bc0b56f092>
2012-04-26 02:35:14 +0000

    수정

--- moviesystem/src/kr/or/kosta/moviesystem/movie/MovieDAO.java
+++ moviesystem/src/kr/or/kosta/moviesystem/movie/MovieDAO.java
@@ -9,32 +9,18 @@ import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 
 
-public class MovieDAO implements IMovieDAO {
-	private static String resource="sqlmap-config.xml";
-	private static Reader sqlReader;
-	static{
-			try {
-				sqlReader=Resources.getResourceAsReader(resource);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-	}
-	private static SqlSessionFactory sqlMapper =
-			new SqlSessionFactoryBuilder().build(sqlReader);
+public class MovieDAO extends SqlSessionDaoSupport implements IMovieDAO {
 	
 	@Override
 	public List selectMovieList(){
 		SqlSession session = null;
 		List<Movie>movieList=null;
-		try{
-			session= sqlMapper.openSession(true);
-			movieList=session.selectList("MovieService.selectMovieList");
-			
-		}finally{
-			session.close();
-		}
+		session= getSqlSession();
+		movieList=session.selectList("MovieService.selectMovieList");
+		
 		return movieList;
 	}
 	@Override
@@ -42,20 +28,16 @@ public class MovieDAO implements IMovieDAO {
 		SqlSession session = null;
 		List<Movie> movieList = null;
 		
-		try{
-			session = sqlMapper.openSession(true);
-			RowBounds rowBounds = new RowBounds((page-1)*length, length);
-			if("screen".equals(gubun)){
-				movieList = session.selectList("MovieService.selectMovieListScreen", null, rowBounds);
-			}else if("schedule".equals(gubun)){
-				movieList = session.selectList("MovieService.selectMovieListSchedule", null, rowBounds);
-			}else if("reservation".equals(gubun)){
-				movieList = session.selectList("MovieService.selectMovieListReservation", null, rowBounds);
-			}else{
-				movieList = session.selectList("MovieService.selectMovieListTotal", null, rowBounds);
-			}
-		}finally{
-			session.close();
+		session = getSqlSession();
+		RowBounds rowBounds = new RowBounds((page-1)*length, length);
+		if("screen".equals(gubun)){
+			movieList = session.selectList("MovieService.selectMovieListScreen", null, rowBounds);
+		}else if("schedule".equals(gubun)){
+			movieList = session.selectList("MovieService.selectMovieListSchedule", null, rowBounds);
+		}else if("reservation".equals(gubun)){
+			movieList = session.selectList("MovieService.selectMovieListReservation", null, rowBounds);
+		}else{
+			movieList = session.selectList("MovieService.selectMovieListTotal", null, rowBounds);
 		}
 		return movieList;
 	}
@@ -63,45 +45,36 @@ public class MovieDAO implements IMovieDAO {
 	public int selectMovieCount(String gubun){
 		SqlSession session = null;
 		Integer mcount = 0;
-		try{
-			session = sqlMapper.openSession(true);
-			if("screen".equals(gubun)){
-				mcount = session.selectOne("MovieService.selectMovieListScreenCnt");
-			}else if("schedule".equals(gubun)){
-				mcount = session.selectOne("MovieService.selectMovieListScheduleCnt");
-			}else if("reservation".equals(gubun)){
-				mcount = session.selectOne("MovieService.selectMovieListReservationCnt");
-			}else{	
-				mcount = session.selectOne("MovieService.selectMovieListTotalCnt");
-			}
-		}finally{
-			session.close();
+		session = getSqlSession();
+		if("screen".equals(gubun)){
+			mcount = session.selectOne("MovieService.selectMovieListScreenCnt");
+		}else if("schedule".equals(gubun)){
+			mcount = session.selectOne("MovieService.selectMovieListScheduleCnt");
+		}else if("reservation".equals(gubun)){
+			mcount = session.selectOne("MovieService.selectMovieListReservationCnt");
+		}else{	
+			mcount = session.selectOne("MovieService.selectMovieListTotalCnt");
 		}
+		
 		return mcount;
 	}	
 	@Override
 	public List<Movie> rankingMovieList(){
 		SqlSession session = null;
 		List<Movie> movieList = null;
-		try{
-			session = sqlMapper.openSession(true);
-			RowBounds rowBounds = new RowBounds(0,3);
-			movieList = session.selectList("MovieService.selectMovieRanking", null, rowBounds);
-		}finally{
-			session.close();
-		}
+		session = getSqlSession();
+		RowBounds rowBounds = new RowBounds(0,3);
+		movieList = session.selectList("MovieService.selectMovieRanking", null, rowBounds);
+		
 		return movieList;
 	}
 	@Override
 	public Movie selectMovie(String mnum){
 		SqlSession session = null;
 		Movie movie = null;
-		try{
-			session = sqlMapper.openSession(true);
-			movie = session.selectOne("MovieService.selectMovie",mnum);
-		}finally{
-			session.close();
-		}
+		session = getSqlSession();
+		movie = session.selectOne("MovieService.selectMovie",mnum);
+		
 		return movie;
 	}
 	@Override
@@ -109,20 +82,17 @@ public class MovieDAO implements IMovieDAO {
 		SqlSession session = null;
 		List<Movie> movieList = null;
 		String exeQuery = null;
-		try{
-			session = sqlMapper.openSession(true);
-			RowBounds rowBounds = new RowBounds((page-1)*length, length);
-			if("mname".equals(schCode)){
-				exeQuery = "MovieService.selectMovieListByMname";
-			}else if("genre".equals(schCode)){
-				exeQuery = "MovieService.selectMovieListByGenre";
-			}else if("content".equals(schCode)){
-				exeQuery = "MovieService.selectMovieListByContent";
-			}
-			movieList = session.selectList(exeQuery,"%"+schString+"%", rowBounds);
-		}finally{
-			session.close();
+		session = getSqlSession();
+		RowBounds rowBounds = new RowBounds((page-1)*length, length);
+		if("mname".equals(schCode)){
+			exeQuery = "MovieService.selectMovieListByMname";
+		}else if("genre".equals(schCode)){
+			exeQuery = "MovieService.selectMovieListByGenre";
+		}else if("content".equals(schCode)){
+			exeQuery = "MovieService.selectMovieListByContent";
 		}
+		movieList = session.selectList(exeQuery,"%"+schString+"%", rowBounds);
+		
 		return movieList;
 	}
 	@Override
@@ -130,61 +100,43 @@ public class MovieDAO implements IMovieDAO {
 		SqlSession session = null;
 		Integer mcnt = 0;
 		String schQuery = null;
-		try{
-			session = sqlMapper.openSession(true);
-			if("mname".equals(schCode)){
-				schQuery = "MovieService.selectMovieListByMnameCount";
-			}else if("genre".equals(schCode)){
-				schQuery = "MovieService.selectMovieListByGenreCount";
-			}else if("content".equals(schCode)){
-				schQuery = "MovieService.selectMovieListByContentCount";
-			}
-			mcnt = session.selectOne(schQuery,"%"+schString+"%");
-		}finally{
-			session.close();
+		session = getSqlSession();
+		if("mname".equals(schCode)){
+			schQuery = "MovieService.selectMovieListByMnameCount";
+		}else if("genre".equals(schCode)){
+			schQuery = "MovieService.selectMovieListByGenreCount";
+		}else if("content".equals(schCode)){
+			schQuery = "MovieService.selectMovieListByContentCount";
 		}
+		mcnt = session.selectOne(schQuery,"%"+schString+"%");
+	
 		return mcnt;
 	}
 	@Override
 	public void addMovie(Movie movie){
 		SqlSession session = null;
-		try{
-			session = sqlMapper.openSession(true);
-			session.insert("MovieService.addMovie",movie);
-		}finally{
-			session.close();
-		}
+		session = getSqlSession();
+		session.insert("MovieService.addMovie",movie);
 	}
 	@Override
 	public void editMovie(Movie movie){
 		SqlSession session = null;
-		try{
-			session = sqlMapper.openSession(true);
-			session.update("MovieService.editMovie",movie);
-		}finally{
-			session.close();
-		}
+		session = getSqlSession();
+		session.update("MovieService.editMovie",movie);
 	}
 	@Override
 	public void removeMovie(String mnum){
 		SqlSession session = null;
-		try{
-			session = sqlMapper.openSession(true);
-			session.delete("MovieService.removeMovie",mnum);
-		}finally{
-			session.close();
-		}
+		session = getSqlSession();
+		session.delete("MovieService.removeMovie",mnum);
 	}
 	@Override
 	public Movie selectMovieNum(String Mname){
 		SqlSession session = null;
 		Movie movie = null;
-		try{
-			session = sqlMapper.openSession(true);
-			movie = session.selectOne("MovieService.selectMovieNum",Mname);
-		}finally{
-			session.close();
-		}
+		session = getSqlSession();
+		movie = session.selectOne("MovieService.selectMovieNum",Mname);
+		
 		return movie;
 	}
 }
