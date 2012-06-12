37b09151676330ac1afef3ac0d2a632537a351c8
kimkijeong1@naver.com <kimkijeong1@naver.com@31618103-7477-875a-9d44-c0bc0b56f092>
2012-06-12 08:02:09 +0000

    reservation 커밋

---
+++ moviesystem/src/kr/or/kosta/moviesystem/reservation/IReservationDAO.java
@@ -0,0 +1,41 @@
+package kr.or.kosta.moviesystem.reservation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public interface IReservationDAO {
+
+	void insertReservation(Reservation reservation);
+
+	List<Reservation> selectReservationList(int length, int page,
+			String memberid);
+
+	List<Reservation> selectReservationTime(String userid, String mnum);
+
+	ArrayList<Reservation> selectReservationList(String memberid);
+
+	int selectReservationCount(String userid);
+
+	long selectReservationSeatNum(long Res_num);
+
+	long selectReservationQty(long res_num);
+
+	String selectReservationResNum(String scr_num);
+
+	Reservation selectReservation(String resnum);
+
+	void removeReservation(String resnum);
+
+	void updateReservation(Reservation reservation);
+
+	void cancelReservation(String resnum);
+
+	List<Reservation> selectSeatNumByScrnumAndUserid(String scrnum,
+			String userid);
+
+	List selectSeatNumByScrnum(String scrnum);
+
+	List selectTotalList(String scrnum);
+	
+}
