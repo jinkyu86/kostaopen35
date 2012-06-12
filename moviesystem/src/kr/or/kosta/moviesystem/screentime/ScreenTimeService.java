bbbf4bb930dfb0cf09a54fa5b5993642150d3ca4
kimkijeong1@naver.com <kimkijeong1@naver.com@31618103-7477-875a-9d44-c0bc0b56f092>
2012-06-12 08:04:23 +0000

    screentime 수정

--- moviesystem/src/kr/or/kosta/moviesystem/screentime/ScreenTimeService.java
+++ moviesystem/src/kr/or/kosta/moviesystem/screentime/ScreenTimeService.java
@@ -3,22 +3,32 @@ package kr.or.kosta.moviesystem.screentime;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import kr.or.kosta.moviesystem.member.Member;
 
 import com.opensymphony.xwork2.ModelDriven;
 
 /**
  * Servlet implementation class ScreenTimeService
  */
-public class ScreenTimeService implements ModelDriven {
+public class ScreenTimeService implements ModelDriven,SessionAware {
 	private static final long serialVersionUID = 1L;
 	private List<ScreenTime> SCREENTIME_LIST ;
 	private String mnum;
+	private String mname;
+	private Map session; 
+	private Member member=new Member();
+	
 	
 	@Override
 	public Object getModel() {
@@ -28,7 +38,43 @@ public class ScreenTimeService implements ModelDriven {
   
 	
 	
-    public List<ScreenTime> getSCREENTIME_LIST() {
+    public Member getMember() {
+		return member;
+	}
+
+
+
+	public void setMember(Member member) {
+		this.member = member;
+	}
+
+
+
+	public Map getSession() {
+		return session;
+	}
+
+
+
+	public void setSession(Map session) {
+		this.session = session;
+	}
+
+
+
+	public String getMname() {
+		return mname;
+	}
+
+
+
+	public void setMname(String mname) {
+		this.mname = mname;
+	}
+
+
+
+	public List<ScreenTime> getSCREENTIME_LIST() {
 		return SCREENTIME_LIST;
 	}
 
@@ -61,7 +107,14 @@ public class ScreenTimeService implements ModelDriven {
     	System.out.println("viewScreenTimeListBymnum실행 ");
     	System.out.println("영화를 선택해서 해당 영화의 시간을 보여주기 위한 곳");
 		System.out.println("mnum==== "+mnum+"==");
+		
+//		Member member=new Member();
+		member.setUserid("1");
+		session.put("LOGIN_MEMBER",member);
 		SCREENTIME_LIST=ScreenTimeDAO.selectScreen(mnum);
+		ScreenTime SCREEN_LIST_MNAME ;
+		SCREEN_LIST_MNAME=SCREENTIME_LIST.get(0);
+		mname=SCREEN_LIST_MNAME.getMovie().getMname();
 		System.out.println("SCREENTIME_LIST= "+SCREENTIME_LIST);
 		System.out.println("viewScreenTimeListBymnum종료 ");
 		return "success";
