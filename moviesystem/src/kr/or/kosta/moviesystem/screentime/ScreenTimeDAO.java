40c19d25d3b8fe7e45b6bba177436051ffd0ec15
chieka@naver.com <chieka@naver.com@31618103-7477-875a-9d44-c0bc0b56f092>
2012-06-20 03:03:13 +0000

    modify

--- moviesystem/src/kr/or/kosta/moviesystem/screentime/ScreenTimeDAO.java
+++ moviesystem/src/kr/or/kosta/moviesystem/screentime/ScreenTimeDAO.java
@@ -2,49 +2,25 @@ package kr.or.kosta.moviesystem.screentime;
 
 import java.io.IOException;
 import java.io.Reader;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 
-import kr.or.kosta.moviesystem.movie.Movie;
-import kr.or.kosta.moviesystem.reservation.Reservation;
-import kr.or.kosta.moviesystem.util.ConnectionUtil;
-
-public class ScreenTimeDAO {
-	private static String resource="sqlmap-config.xml";
-	private static Reader sqlReader;
-	static{
-			try {
-				sqlReader=Resources.getResourceAsReader(resource);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-	}
-	private static SqlSessionFactory sqlMapper =
-			new SqlSessionFactoryBuilder().build(sqlReader);
-	
-	
-	
-	
-	
-	
-	
+public class ScreenTimeDAO  extends SqlSessionDaoSupport implements IScreenTimeDAO{	
 	/**
 	 * 상영시간 등록 기능
 	 * 
 	 * @param time
 	 */
+	@Override
 	public void insertScreenTime(Date time) {
-		/* default generated stub */;
+		// TODO Auto-generated method stub
 		
 	}
 	/**
@@ -52,19 +28,18 @@ public class ScreenTimeDAO {
 	 * 
 	 * @param time
 	 */
-	public static ScreenTime selectScreenTimeScrNum(String time) {
-		SqlSession session=null;
+	@Override
+	public ScreenTime selectScreenTimeScrNum(String time) {
+		SqlSession session = null;
 		ScreenTime screentime=null;
 		
 		String timesubstring=time.substring(0,19);
 		System.out.println("timesubstring= "+timesubstring);
 		
-		try{
-			session=sqlMapper.openSession(true);
-			screentime=session.selectOne("ScreenTime.selectScreenTimeScrNum", timesubstring);
-		}finally{
-			session.close();
-		}
+		session= getSqlSession();
+		
+		screentime=session.selectOne("ScreenTime.selectScreenTimeScrNum", timesubstring);
+		
 		return screentime;
 	}
 //	public static ScreenTime selectScreenTimeScrNum(String time) {
@@ -128,28 +103,16 @@ public class ScreenTimeDAO {
 	}
 	 * @param mnum
 	 */
-	public static ScreenTime selectScreenTimeBySrcNum(String scrnum) {
-		System.out.println("scrnum="+scrnum);
+	@Override
+	public ScreenTime selectScreenTimeBySrcNum(String scrnum) {
 		SqlSession session=null;
 		ScreenTime screenTime=null;
-		
-		try{
-			session = sqlMapper.openSession(true);
-			screenTime=session.selectOne("ScreenTime.selectScreenTime",scrnum);
-			System.out.println("scrnum="+scrnum);
-		}
-		
-		finally{
-			//Connection을 ConnectionPool에 반납해야지 안끊긴다. 
-			//그리고 이건 무조건 반납해야지 멈추지 않으니 finally에 써주어야한다.
-			session.close();
-		}
+		session= getSqlSession();
+		screenTime=session.selectOne("ScreenTime.selectScreenTime",scrnum);
+		System.out.println("scrnum="+scrnum);
 		return screenTime;
 	}
-	
-	
-	
-	
+
 	
 	/**
 	 * 영화이름으로 상영시간을 찾을 수 있는 기능
@@ -173,63 +136,14 @@ public class ScreenTimeDAO {
 	}
 	 * @param mnum
 	 */
-	public static List<ScreenTime> selectScreen(String mnum) {
+	@Override
+	public List<ScreenTime> selectScreen(String mnum) {
 		SqlSession session=null;
 		List<ScreenTime> screenTimeList=null;
-		try{
-			session = sqlMapper.openSession(true);
-			screenTimeList=
-				session.selectList("ScreenTime.selectScreenList",mnum);
+		session= getSqlSession();
+		screenTimeList= session.selectList("ScreenTime.selectScreenList",mnum);
 		
-
-		}
-		
-		finally{
-			//Connection을 ConnectionPool에 반납해야지 안끊긴다. 
-			//그리고 이건 무조건 반납해야지 멈추지 않으니 finally에 써주어야한다.
-			session.close();
-		}
 		return screenTimeList;
 	}
 
-	/**
-	 * 전체 상영시간 조회 기능
-	 * 
-	 * @param time
-	 */
-	public ArrayList selectScreenTimeList() {
-		Connection con=null;
-		PreparedStatement psmt=null;
-		String sql=null;
-		ResultSet rs=null;
-		ScreenTime screentime=null;
-		ArrayList<ScreenTime>screentimeList=new ArrayList<ScreenTime>();
-		try {
-			con=ConnectionUtil.getConnection();
-			sql="SELECT m.m_name,s.time" +
-					
-					"  FROM  MOVIE m,SCREENING_TIME s " +
-					"  WHERE  s.m_num=m.m_num ";
-			
-			
-				psmt=con.prepareStatement(sql);
-				rs=psmt.executeQuery();
-				while(rs.next()){
-					String m_name=rs.getString(1);
-					String time=rs.getString(2);
-								
-					Movie movie=new Movie();
-					screentime=new ScreenTime();
-					
-					movie.setMname(m_name);
-					screentime.setMovie(movie);
-					screentime.setTime(time);					
-				
-					screentimeList.add(screentime);
-				}//end while
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return screentimeList;
-	}
 }
